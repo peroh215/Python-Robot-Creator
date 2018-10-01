0f24e58df43ec86f1b5f51d861f09fbbdb09454f
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-10-01 00:16:00 -0300

    Update 3.0
    Added a delete existing robot feature and changed some stuff

--- robot_creator.py
+++ robot_creator.py
@@ -1,27 +1,25 @@
 # /usr/bin/python
 #-*- coding: utf-8 -*-
 
-print("Loading...")
-
 # Variables
 usr = ''
 helplist = '''--- Commands ---
 !create - Creates a new robot
 !list - Displays a list of created robots
 !show - Shows information about a specific robot
+!delete - Deletes an existing robot
 !about - Displays information about the program'''
 robots = []
 rid = 1
 aboutmsg = '''
 ===============================
 File name: robot_creator
-Version: 1.0
+Version: 3.0
 Author: Blackman White                     
 Date created: 9/30/2018              
-Date last modified: 9/30/2018        
+Date last modified: 10/1/2018        
 Python Version: 3.7
-=================================
-'''
+================================='''
 
 # Functions
 class Robot:
@@ -39,6 +37,9 @@ class Robot:
         print("ID:", self.id)
         print("------------------------------")
 
+    def __del__(self):
+        print("{} has been deleted".format(self.name))
+
 def menu():
     global usr
     usr_i()
@@ -55,12 +56,15 @@ def menu():
     elif usr == "!show":
         show_r()
         menu()
+    elif usr == "!delete":
+        delete_r()
+        menu()
     elif usr == "!about":
         print(aboutmsg)
         menu()
     else:
         print("Invalid command")
-        print("Type !help to see commands")
+        print("Type !help to see commands\n")
         menu()
         
 def usr_i():
@@ -71,9 +75,17 @@ def create_r():
     global usr
     print("\n===== Robot Creator =====")
     a = input("Name: ")
+    
+    for robot in robots:
+        if a == robot.name:
+            print("A robot with that name already exists, delete the existing robot or pick another name")
+        else:
+            return None
+        
     b = input("Color: ")
     r = Robot(a,b)
     robots.append(r)
+    
 
 def list_r():
     global robots
@@ -101,9 +113,28 @@ def show_r():
         
     if robot_found == False:
         print("Robot not found")
+
+def delete_r():
+    global robots
+    if not robots:
+        print("There are no robots existent, type !create to create a robot")
+        return None
+    usr = input("Enter robot name: ")
+    robot_found = False
+        
+    for robot in robots:
+        if usr == robot.name:
+            robot_found = True
+            robots.remove(robot)
+            del robot
+            return None
+        
+    if robot_found == False:
+        print("Robot not found")
     
 # Main
-print("===== Menu =====")
+print("========== Menu ==========")
 print("Type !help to see commands")
 menu()
+
 
