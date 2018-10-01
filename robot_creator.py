112289bb08782dc4c5d424fa44966a1ba33674d7
Ducc <43486978+BlackmanWhite@users.noreply.github.com>
2018-09-30 23:42:09 -0300

    Update 2.0

--- robot_creator.py
+++ robot_creator.py
@@ -7,22 +7,37 @@ print("Loading...")
 usr = ''
 helplist = '''--- Commands ---
 !create - Creates a new robot
-!show - Shows created robots '''
+!list - Displays a list of created robots
+!show - Shows information about a specific robot
+!about - Displays information about the program'''
 robots = []
 rid = 1
+aboutmsg = '''
+===============================
+File name: robot_creator
+Version: 1.0
+Author: Blackman White                     
+Date created: 9/30/2018              
+Date last modified: 9/30/2018        
+Python Version: 3.7
+=================================
+'''
 
 # Functions
 class Robot:
-    def __init__(self, name):
+    def __init__(self, name, color):
         global rid
         self.name = name
+        self.color = color
         self.id = rid
         rid += 1
 
-    def robot_info(self):
-        print("----- Robot info -----")
+    def info(self):
+        print("\n----- {}'s information -----".format(self.name))
         print("Name:", self.name)
+        print("Color:", self.color)
         print("ID:", self.id)
+        print("------------------------------")
 
 def menu():
     global usr
@@ -33,8 +48,16 @@ def menu():
         menu()
     elif usr == "!create":
         create_r()
+        menu()
+    elif usr == "!list":
+        list_r()
+        menu()
     elif usr == "!show":
         show_r()
+        menu()
+    elif usr == "!about":
+        print(aboutmsg)
+        menu()
     else:
         print("Invalid command")
         print("Type !help to see commands")
@@ -47,19 +70,40 @@ def usr_i():
 def create_r():
     global usr
     print("\n===== Robot Creator =====")
-    a = input("Name:")
-    r = Robot(a)
+    a = input("Name: ")
+    b = input("Color: ")
+    r = Robot(a,b)
     robots.append(r)
-    menu()
 
-def show_r():
+def list_r():
+    global robots
+    if not robots:
+        print("There are no robots existent, type !create to create a robot")
+        return None
     print("------ Robots ------\n")
     for robot in robots:
-        print("{} - {}".format(robot.id,robot.name))
-    print("--------------------")
-    menu()
+        print("[{}] {}".format(robot.id,robot.name))
+    print("\n--------------------")
+
+def show_r():
+    global robots
+    if not robots:
+        print("There are no robots existent, type !create to create a robot")
+        return None
+    usr = input("Enter robot name: ")
+    robot_found = False
+        
+    for robot in robots:
+        if usr == robot.name:
+            robot_found = True
+            robot.info()
+            return None
+        
+    if robot_found == False:
+        print("Robot not found")
     
 # Main
 print("===== Menu =====")
 print("Type !help to see commands")
 menu()
+
